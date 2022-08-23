a8bd89ef250dab258a01731c5a7253f85729d5fc
mbellazhang <104393765+mbellazhang@users.noreply.github.com>
2022-08-23 13:07:49 +0100

    food order day 6

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,15 +7,19 @@ const Cart = (props) => {
   const cartCtx = useContext(CartContext)
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
   const hasItems = cartCtx.items.length > 0
-  const cartItemRemoveHandler = (id) => { }
-  const cartItemAddHandler = (item) => { }
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id)
+  }
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 })
+  }
   const cartItems = <ul className="cart-items">{cartCtx.items.map((item) => <CartItem
     key={item.id}
     name={item.name}
     price={item.price}
     amount={item.amount}
     onRemove={cartItemRemoveHandler.bind(null, item.id)}
-    onAdd={cartItemAddHandler.bind(item)}
+    onAdd={cartItemAddHandler.bind(null, item)}
 
   />)}</ul>
   return (
